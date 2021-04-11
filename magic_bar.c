cc6c92bf47cf6370a05624fe0bd0cbe5e91631be
Mathieu Delacroix <mathieu.delacroix.fr@gmail.com>
2021-04-11 22:30:26 +0200

    remove useless comments

--- magic_bar.c
+++ magic_bar.c
@@ -12,5 +12,4 @@ int main(int argc, char **argv)
     t_config  config = parse_config(CONFIG_PATHNAME);
     t_gtk_ui  ui = setup_ui();
     return start_ui(&ui, &config, &run_command, argc, argv);
-    // return run_command(config.default_command);
 }
